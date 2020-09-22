96daa1557bb30eb5aad1423f04e9802bae3a141c
kozzzz-07 <kozzzz-07@noreply>
2020-09-22 23:40:45 +0900

    フォルダ構成ざっくり

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
+import { TodoListComponent } from './feature-modules/todo/components/presentations/todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
