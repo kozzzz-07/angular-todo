6bf33ef266dc53b0b70732fb86f19b4c3c0bcc1d
kozzzz-07 <kozzzz-07@noreply>
2020-09-22 13:43:12 +0900

    todoっぽいやつ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-todo';
+  tasks: Todo[] = [];
+  newTaskTitle = '';
+
+  addTask(): void {
+    this.tasks.push({ title: this.newTaskTitle, isDone: false });
+    this.newTaskTitle = '';
+  }
 }
+
+type Todo = { title: string; isDone: boolean };
